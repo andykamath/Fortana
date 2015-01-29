8b328b0635668c395a6a21ebcd0474f6291ae503
Andy Kamath <andy@andykamath.net>
2015-01-29 13:50:30 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     {
         $city = substr($end, 11);
         $country = "US";
-        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+        $url="http://api.openweathermap.org/data/2.5/weather?q=Charlotte,NC&units=metric&cnt=7&lang=en";
         $json=file_get_contents($url);
         $data=json_decode($json,true);
         $end = $data['weather'][0]['main']."<br>";
