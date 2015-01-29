ccbf2ab9822685a57963524540b511f903250697
Andy Kamath <andy@andykamath.net>
2015-01-29 13:45:16 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,11 @@
         $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
         $json=file_get_contents($url);
         $data=json_decode($json,true);
-        echo $data['weather'][0]['main']."<br>";
-        $end = 
+        $end = $data['weather'][0]['main']."<br>";
+    }
+    else
+    {
+        $end = "Oops something went wrong";
     }
 ?>
 <Response>
