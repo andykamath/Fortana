7742566be6fd7e793e18ec5de70c2db3d6751402
Andy Kamath <andy@andykamath.net>
2015-01-29 17:57:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     $a = strtolower($_REQUEST['Body']);
-    if($a == "weather")
-    {
-        $city = substr($end, 11);
-        $country = "US";
-        $url="http://api.openweathermap.org/data/2.5/weather?q=Charlotte,NC&units=metric&cnt=7&lang=en";
-        $json=file_get_contents($url);
-        $data=json_decode($json,true);
-        $end = $data['weather'][0]['main']."<br>";
-    }
-    else
-    {
-        $end = "Oops something went wrong";
-    }
+    $end = $a
 ?>
 <Response>
     <Message><?php echo $end; ?></Message>
