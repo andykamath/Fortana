1226c5790a0027cd6c64cf65faedcde2a2950144
Andy Kamath <andy@andykamath.net>
2015-01-29 14:09:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    $a = $_REQUEST['Body'];
+    $a = strtolower($_REQUEST['Body']);
     if($a == "weather")
     {
         $city = substr($end, 11);
