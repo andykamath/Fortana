5d3557fe117fb6ce5bf5df854f1ff3a8d05480f9
Andy Kamath <andy@andykamath.net>
2015-01-29 13:38:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    $a = $_REQUEST['Body'];
+    if(strpos($a, "weather")==true)
+    {
+        $city = substr($end, 11);
+        $country = "US";
+        $url="http://api.openweathermap.org/data/2.5/weather?q=".$city.",".$country."&units=metric&cnt=7&lang=en";
+        $json=file_get_contents($url);
+        $data=json_decode($json,true);
+        echo $data['weather'][0]['main']."<br>";
+        $end = 
+    }
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message><?php echo $end; ?></Message>
 </Response>
