956ca016c15ebea562ab892e66f83b72f48fa9b7
Andy Kamath <andy@hackgician.net>
2015-01-29 20:10:13 -0500

    Swag

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    require('./wordnik/Swagger.php');
+    require('wordnik/Swagger.php');
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     $a = strtolower($_REQUEST['Body']);
