b5e66e600511f966b6b86813a81099bc7e7778fb
Andy Kamath <andy@hackgician.net>
2015-02-06 17:19:08 -0500

    SWAG

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
   include '../wa_wrapper/WolframAlphaEngine.php';
-  $request = 'http://api.wolframalpha.com/v2/query?appid=YKWERR-FJJAE7L2L6&input=albert%20einstein&format=html';
+  $request = 'http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=albert%20einstein&format=html';
 
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
