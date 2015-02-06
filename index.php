13ea6dc65d02243e7f31c3f2b55bc711878b4405
Andy Kamath <andy@hackgician.net>
2015-02-06 18:28:50 -0500

    SWAG

--- index.php
+++ index.php
@@ -27,6 +27,24 @@
     {
         $end = calculate_string(substr($a, 5));
     }
+    else if (strpos($a, 'who is') !== false) {
+        $word = substr($a, 7);
+        $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$word."&format=plaintext&podtitle=Notable%20facts";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        ob_start();
+        curl_exec($ch);
+        curl_close($ch);
+        $contents = ob_get_contents();
+        ob_end_clean();
+        function get($a,$b,$c){
+            $y = explode($b,$a);
+            $x = explode($c,$y[1]);
+            return $x[0];
+        }
+        echo get($contents,'<plaintext>','</plaintext>');
+
+    }
     else
     {
         $end = "Oops something went wrong";
--- test.php
+++ test.php
@@ -1,19 +1,2 @@
 <?php
-$url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=Albert%20Einstein&format=plaintext&podtitle=Notable%20facts";
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-ob_start();
-curl_exec($ch);
-curl_close($ch);
-$contents = ob_get_contents();
-ob_end_clean();
-
-function get($a,$b,$c){
-     $y = explode($b,$a);
-     $x = explode($c,$y[1]);
-    return $x[0];
-}
-
-echo get($contents,'<plaintext>','</plaintext>');
 ?>
