9db8728e58cce89efe201932a1116c4dd9009af7
Andy Kamath <andy@hackgician.net>
2015-02-08 15:43:12 -0500

    SWAG

--- text.php
+++ text.php
@@ -4,99 +4,142 @@
     $a = strtolower($_REQUEST['Body']);
     if (strpos($a, 'weather') !== false)
     {
-        $city = substr($end, 11);
-        $country = "US";
-        $url="http://api.openweathermap.org/data/2.5/weather?q=Charlotte,US&units=metric&cnt=7&lang=en";
-        $json=file_get_contents($url);
-        $data=json_decode($json,true);
-        $temp = intval($data['main']['temp']);
-        $temp = $temp* 2 + 32.00;
-        $end = $data['weather'][0]['description']." and the temperature is ".$temp;
+        try
+        {
+            $city = substr($end, 11);
+            $country = "US";
+            $url="http://api.openweathermap.org/data/2.5/weather?q=Charlotte,US&units=metric&cnt=7&lang=en";
+            $json=file_get_contents($url);
+            $data=json_decode($json,true);
+            $temp = intval($data['main']['temp']);
+            $temp = $temp* 2 + 32.00;
+            $end = $data['weather'][0]['description']." and the temperature is ".$temp;
+        }
+        catch(Exception $e)
+        {
+            $end = "Sorry, something went wrong with your request. Please make sure your syntax is correct, i.e. \"What's the weather like outside\" or simply, \"Weather\"";
+        }
     }
     else if (strpos($a, 'define ') !== false)
     {
-        $word = substr($a, 7);
-        $url = "http://api.wordnik.com/v4/word.json/".$word."/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
-        $json=file_get_contents($url);
-        $data = json_decode($json, true);
-        $def = $data[0]['text'];
-        $end = $def;
+        try
+        {
+            $word = substr($a, 7);
+            $url = "http://api.wordnik.com/v4/word.json/".$word."/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+            $json=file_get_contents($url);
+            $data = json_decode($json, true);
+            $def = $data[0]['text'];
+            $end = $def;
+        }
+        catch (Exception $e)
+        {
+            $end = "Sorry, but unfortunately that word is not defined in our dictionary. Try asking, \"What is...\" to get a better answer";
+        }
     }
     else if (strpos($a, 'calc ') !== false)
     {
-        $end = calculate_string(substr($a, 5));
+        try {
+            $end = calculate_string(substr($a, 5));   
+        } catch (Exception $e) {
+            $end = "Sorry, that syntax was not caught. This calculator can only do addition, 1+1, subtraction, 1-1, multiplication, 1*1, or division, 1/1.";
+        }
     }
     else if (strpos($a, 'who is') !== false) {
-        $word = substr($a, 7);
-        $final = str_replace(" ", "%20", $word);
-        $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Notable%20facts";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        ob_start();
-        curl_exec($ch);
-        curl_close($ch);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        function get($a,$b,$c){
-            $y = explode($b,$a);
-            $x = explode($c,$y[1]);
-            return $x[0];
+        try
+        {
+            $word = substr($a, 7);
+            $final = str_replace(" ", "%20", $word);
+            $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Notable%20facts";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            ob_start();
+            curl_exec($ch);
+            curl_close($ch);
+            $contents = ob_get_contents();
+            ob_end_clean();
+            function get($a,$b,$c){
+                $y = explode($b,$a);
+                $x = explode($c,$y[1]);
+                return $x[0];
+            }
+            $end = get($contents,'<plaintext>','</plaintext>');
         }
-        $end = get($contents,'<plaintext>','</plaintext>');
-    }
+        catch(Exception $e)
+        {
+            $end = "Sorry, that person isn't in our encyclopedia";
+        }
     else if (strpos($a, 'who was') !== false) {
-        $word = substr($a, 8);
-        $final = str_replace(" ", "%20", $word);
-        $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Notable%20facts";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        ob_start();
-        curl_exec($ch);
-        curl_close($ch);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        function get($a,$b,$c){
-            $y = explode($b,$a);
-            $x = explode($c,$y[1]);
-            return $x[0];
+        try
+        {
+            $word = substr($a, 8);
+            $final = str_replace(" ", "%20", $word);
+            $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Notable%20facts";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            ob_start();
+            curl_exec($ch);
+            curl_close($ch);
+            $contents = ob_get_contents();
+            ob_end_clean();
+            function get($a,$b,$c){
+                $y = explode($b,$a);
+                $x = explode($c,$y[1]);
+                return $x[0];
+            }
+            $end = get($contents,'<plaintext>','</plaintext>');
         }
-        $end = get($contents,'<plaintext>','</plaintext>');
+        catch(Exception $e)
+        {
+            $end = "Sorry, that person isn't in our encyclopedia";
+        }
     }
     else if (strpos($a, 'what is ') !== false) {
-        $word = substr($a, 8);
-        $final = str_replace(" ", "%20", $word);
-        $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Definitions";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        ob_start();
-        curl_exec($ch);
-        curl_close($ch);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        function get($a,$b,$c){
-            $y = explode($b,$a);
-            $x = explode($c,$y[1]);
-            return $x[0];
+        try
+        {
+            $word = substr($a, 8);
+            $final = str_replace(" ", "%20", $word);
+            $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Definitions";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            ob_start();
+            curl_exec($ch);
+            curl_close($ch);
+            $contents = ob_get_contents();
+            ob_end_clean();
+            function get($a,$b,$c){
+                $y = explode($b,$a);
+                $x = explode($c,$y[1]);
+                return $x[0];
+            }
+            $end = get($contents,'<plaintext>','</plaintext>');
+        catch(Exception $e)
+        {
+            $end = "Sorry, that wasn't found in our Encyclopedia. Try saying \"Define...\" to get a better answer";
         }
-        $end = get($contents,'<plaintext>','</plaintext>');
     }
     else if (strpos($a, 'what was ') !== false) {
-        $word = substr($a, 8);
-        $final = str_replace(" ", "%20", $word);
-        $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Definitions";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        ob_start();
-        curl_exec($ch);
-        curl_close($ch);
-        $contents = ob_get_contents();
-        ob_end_clean();
-        function get($a,$b,$c){
-            $y = explode($b,$a);
-            $x = explode($c,$y[1]);
-            return $x[0];
+        try
+        {
+            $word = substr($a, 9);
+            $final = str_replace(" ", "%20", $word);
+            $url = "http://api.wolframalpha.com/v2/query?appid=UHAP2X-235A2GLHLQ&input=".$final."&format=plaintext&podtitle=Definitions";
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            ob_start();
+            curl_exec($ch);
+            curl_close($ch);
+            $contents = ob_get_contents();
+            ob_end_clean();
+            function get($a,$b,$c){
+                $y = explode($b,$a);
+                $x = explode($c,$y[1]);
+                return $x[0];
+            }
+            $end = get($contents,'<plaintext>','</plaintext>');
+        catch(Exception $e)
+        {
+            $end = "Sorry, that wasn't found in our Encyclopedia. Try saying \"Define...\" to get a better answer";
         }
-        $end = get($contents,'<plaintext>','</plaintext>');
     }
     else
     {
